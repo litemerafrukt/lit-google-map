fb53c4fa63aefba66c2cff9d9a624e0dfa85cce8
Anders Nygren <anders.nygren@parkster.se>
2023-08-10 13:03:36 +0200

    Bump default version

--- src/lit-google-map.ts
+++ src/lit-google-map.ts
@@ -18,7 +18,7 @@ export class LitGoogleMap extends LitElement {
      * Version of the Google Maps API to use.
      */
     @property({type : String})
-    version: string = '3.48';
+    version: string = '3.52';
 
     /**
      * If set, custom styles can be applied to the map.
